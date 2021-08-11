dd763ec5fc1fe2df792c78360bbf6e57d1aed2ab
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-11 15:48:15 -0400

    Added add_attack and get_attack methods.

--- dinosaur.py
+++ dinosaur.py
@@ -4,14 +4,30 @@
 
 
 
+from attacks import Attacks
+
+
 class Dinosaur:
     #Constructor
     def __init__(self, name, attack_power):
         self.name = name
         self.attack_power = attack_power
         self.health = 100
+        self.attack_types = []
 
     #Methods
     def attack(self, robot):
         robot.health -= self.attack_power
+
+    def add_attack(self):
+        chomp = Attacks("Chomp", 30)
+        stomp = Attacks("Stomp", 25)
+        romp = Attacks("Romp", 15)
+        self.attack_types.append(chomp)
+        self.attack_types.append(stomp)
+        self.attack_types.append(romp)
+
+    def get_attack(self, index):
+        print("You selected " + self.attack_types[index].name)
+        return self.attack_types[index]
         
